f2251988f529816c991c9606a2162f7b01ac1273
Brian B. Shao <brianshao9@gmail.com>
2021-09-30 11:59:23 -0500

    Finalized views and necessary routing I believe

--- test_app/config/routes.rb
+++ test_app/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root 'books#index'
 
-  resources :books
+  resources :books do
+    member do
+      get 'delete'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
